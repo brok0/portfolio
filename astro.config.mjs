b74da5559e1b8d92d9a0542bf41e59a8b18a8859
brok0 <48617033+brok0@users.noreply.github.com>
2023-03-31 17:06:49 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({ site: "https://brok0.github.io" });
+export default defineConfig({ site: "https://brok0.github.io", base : '/' });
